342d5f1472227898b0df3c76d3dc5457124b4248
Lansilvester <alan.lengkong@gmail.com>
2023-01-03 22:14:59 +0800

    Register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,15 +1,33 @@
+const Users = require('../models/users');
+const {validationResult} = require('express-validator');
+
 exports.register = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        const err = new Error('Invalid register');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
+
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
      
-    const result = {
-        message: 'Register Success',
-        data:{
-            uid: 1,
-            name: name,
-            email: email,
-        }
-    }
-    res.status(201).json(result);
+    const Register = new Users({
+        name: name,
+        email:email,
+        password:password
+    })
+    Register.save()
+    .then(result => {
+        res.status(201).json({
+            message: 'Register Success',
+            data:result
+        })
+    })
+    .catch(err => {
+        console.log('Err: ', err)
+    })
 }
